191568e1cd63db3a1da92a88c0904dd5f3ca6f2e
Monae <93678860+purplemo340@users.noreply.github.com>
2025-10-15 17:49:02 -0400

    Added paypal and removed stripe

---
+++ my-react-app/client/src/main.jsx
@@ -0,0 +1,14 @@
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1 className='title'>Tarts</h1>
+    <App />
+    
+  </div>
+)
